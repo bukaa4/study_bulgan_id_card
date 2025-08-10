f0c934ffe77519d87d06a8956a009d87f4dc2bf0
bukaa4 <buka.a4@gmail.com>
2025-08-10 15:41:33 +0900

    Modified path in vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/study_my_id_card">
+    <BrowserRouter basename="/bulgan_study">
       <App />
     </BrowserRouter>
   </StrictMode>,
